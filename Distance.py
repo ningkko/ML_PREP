82b26f7d347cc902ff0ee4e05a009d3e41dd62c0
N e n e <makiasagawa@gmail.com>
2020-04-24 02:17:37 -0400

    jaccard dis

--- Distance.py
+++ Distance.py
@@ -1,4 +1,5 @@
 import nltk
+nltk.download('punkt')
 
 
 def getInputs():
@@ -6,20 +7,27 @@ def getInputs():
     if not text1:
         text1 = "But other sources close to the sale said Vivendi was keeping the door open to further bids and hoped " \
                 "to see bidders interested in individual assets team up"
-        print("Default text1: "+text1)
+        print("Default text1: " + text1)
 
     text2 = input("text2: ")
     if not text2:
         text2 = " But other sources close to the sale said Vivendi was keeping the door open for further bids in the " \
                 "next day or two"
-        print("Default text2: "+text2)
+        print("Default text2: " + text2)
 
     return text1, text2
 
 
+def jaccardDistance(text1, text2):
+    # the tokenize function converts a string to a set.
+    # def tokenize(self, s):
+    #    return s.split(self._string)
+
+    # by definition the jaccard distance calculates unique
+    set1 = set(nltk.word_tokenize(text1))
+    set2 = set(nltk.word_tokenize(text2))
+    return nltk.jaccard_distance(set1, set2)
+
+
 def editDistance(text1, text2):
-
-    # token1 = nltk.word_tokenize(text1)
-    # token2 = nltk.word_tokenize(text2)
-
     return nltk.edit_distance(text1, text2)
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-import nltk
-import EditDistance
+import csv
+import Distance
 
 
 def main():
-    text1, text2 = EditDistance.getInputs()
+    text1, text2 = Distance.getInputs()
+    editDistance = str(Distance.editDistance(text1, text2))
+    jaccardDistance = str(Distance.jaccardDistance(text1, text2))
+
     print("++++++++++++++++++++++++++++++++++++\n" +
-          "Edit distance: " + str(EditDistance.editDistance(text1, text2)))
+          "Edit distance: " + editDistance + "\nJaccard distance: " + jaccardDistance)
+
+    with open('output.csv', 'w') as csvfile:
+        pen = csv.writer(csvfile)
+        pen.writerow(['text1'] + ['text2'] + ['edit distance'] + ['Jaccard distance'])
+        pen.writerow([text1, text2, editDistance, jaccardDistance])
+
+    csvfile.close()
 
 
 main()
