2ecb3adfd6e720e35116e51a90747ee5126f53a3
N e n e <makiasagawa@gmail.com>
2020-04-30 21:38:26 -0400

    #(bert==1)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import CSV
 import Distance
 import Sort
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -30,11 +31,22 @@ def main():
         jaccardDistance = Distance.jaccardDistance(sent1, sent2)
         len_sent1 = len(sent1.split(" "))
         len_sent2 = len(sent2.split(" "))
+
+        bert_classification = pairs[2]
+        # number of pairs with ED >= 8 and bert numbers == 1
+        bert_number_1_g8 = 0
+        # number of pairs with ED >= 8
+        ED_number_g8 = 0
+
         lst = [pairs[0], pairs[1], str(jaccardDistance), str(editDistance), pairs[2], pairs[3], len_sent1, len_sent2]
         output.append(lst)
 
         if editDistance >= 8:
             output_E8.append(lst)
+            ED_number_g8 += 1
+
+            if bert_classification == 1:
+                bert_number_1_g8 += 1
 
         if i <= 431:
             original_sentences_len.append(len_sent2)
@@ -56,13 +68,16 @@ def main():
         print(o8)
 
     print("original_median: " + str(original_median) + "\n" +
-          "original_mean: " + str(original_mean))
+          "original_mean: " + str(original_mean) + "\n" +
+          "% of bert == 1: " + str(bert_number_1_g8 / ED_number_g8))
 
     print("Writing results to file...")
 
     txt = open("output.txt", "w")
     txt.write("original_median: " + str(original_median) + "\n" +
-              "original_mean: " + str(original_mean))
+              "original_mean: " + str(original_mean) + "\n" +
+              "% of bert == 1: " + str(bert_number_1_g8 / ED_number_g8))
+
     txt.close()
 
     CSV.write("output.csv", output)
