fab03d04c39b1c794920996a6fd8535303e8855d
N e n e <makiasagawa@gmail.com>
2020-05-06 23:42:13 -0400

    Update mwU_text.py

--- ConceptualDependency/mwU_text.py
+++ ConceptualDependency/mwU_text.py
@@ -23,3 +23,29 @@ print(u_statistic_j)
 
 print('P value:')
 print(pVal_j)
+
+
+# ============== Edit Distance ============================
+
+e0 = []
+with open('../outputs/mwUtest_data/E_bert0.txt') as file:
+    e0 = file.read()
+    e0 = ast.literal_eval(e0)
+file.close()
+
+e1 = []
+with open('../outputs/mwUtest_data/E_bert1.txt') as file:
+    e1 = file.read()
+    e1 = ast.literal_eval(e1)
+file.close()
+
+# Calculate u and probability of a difference
+
+u_statistic_e, pVal_e = stats.mannwhitneyu(e0, e1)
+
+# Print results
+print('E statistics:')
+print(u_statistic_e)
+
+print('P value:')
+print(pVal_e)
